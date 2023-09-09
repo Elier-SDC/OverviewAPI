5d1e8726730ea15efb3a03a19646aacf2efcb942
Heith Mahfoudhi <heith.mhd@gmail.com>
2023-09-09 17:38:06 -0400

    stress test

--- server/app.js
+++ server/app.js
@@ -17,8 +17,13 @@ app.get('/', (request, response) => {
   response.json({ info: 'API for product information' });
 });
 
-const PORT = process.env.PORT || 8080;
 
+
+app.get('/loaderio-3ad75f0536f19a6daeaa3d6266764b22', (req, res) => {
+  res.send('loaderio-3ad75f0536f19a6daeaa3d6266764b22');
+});
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
