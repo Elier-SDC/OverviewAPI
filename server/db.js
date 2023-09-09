c6ecf1dcae7e4ad8721dd4c679d335af1106361e
Heith Mahfoudhi <heith.mhd@gmail.com>
2023-09-09 12:24:14 -0400

    changed db.js to reduce query time to 9ms

--- server/db.js
+++ server/db.js
@@ -3,18 +3,21 @@ const { Pool } = require('pg');
 const fs = require('fs');
 const path = require('path');
 
-const csvDirectory = path.join(__dirname, 'csv_files');
-const schemaPath = path.join(__dirname, 'schema.sql');
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DBPORT,
 });
 
 const connectDB = async () => {
-  await pool.connect();
+  try {
+    await pool.connect();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 };
 
 const closeDB = async () => {
@@ -26,48 +29,31 @@ const closeDB = async () => {
   }
 };
 
-const importCSV = async (tableName, filePath) => {
-  try {
-    const fullPath = path.join(csvDirectory, filePath);
-    const copyQuery = `COPY ${tableName} FROM '${fullPath}' DELIMITER ',' NULL AS 'null' CSV HEADER`;
-
-    await pool.query(copyQuery);
-    console.log(`Data imported into ${tableName} table.`);
-  } catch (error) {
-    console.error(`Error importing data into ${tableName} table:`, error);
-  }
-};
-
-const dropTableIfExists = async (tableName) => {
-  await pool.query(`DROP TABLE IF EXISTS ${tableName} CASCADE`);
-};
-
 const buildDB = async () => {
   try {
-    await connectDB();
-    console.log('Connected to the database');
 
-    // Create the "public" schema if it doesn't exist
-    await pool.query('CREATE SCHEMA IF NOT EXISTS public');
+    await pool.query('DROP TABLE IF EXISTS product CASCADE');
+    await pool.query('DROP TABLE IF EXISTS styles CASCADE');
+    await pool.query('DROP TABLE IF EXISTS skus CASCADE');
+    await pool.query('DROP TABLE IF EXISTS photos CASCADE');
+    await pool.query('DROP TABLE IF EXISTS features CASCADE');
 
-    // Drop existing tables if they exist
-    await dropTableIfExists('product');
-    await dropTableIfExists('styles');
-    await dropTableIfExists('skus');
-    await dropTableIfExists('photos');
-    await dropTableIfExists('features');
 
     // Read the schema.sql file
+    const schemaPath = path.join(__dirname, 'schema.sql');
     const schemaSQL = fs.readFileSync(schemaPath, 'utf8');
 
     // Execute the SQL script to create tables and indexes
     await pool.query(schemaSQL);
 
-    // Import CSVs
-    const csvFileNames = ['product.csv', 'styles.csv', 'features.csv', 'photos.csv', 'skus.csv'];
-    for (const fileName of csvFileNames) {
-      await importCSV(fileName.replace('.csv', ''), fileName);
-    }
+    // COPY existing data into tables
+    await pool.query(`COPY product FROM '${process.env.PRODUCTPATH}' DELIMITER ',' NULL AS 'null' CSV HEADER`);
+    await pool.query(`COPY styles FROM '${process.env.STYLESPATH}' DELIMITER ',' NULL AS 'null' CSV HEADER`);
+    await pool.query(`COPY skus FROM '${process.env.SKUSPATH}' DELIMITER ',' NULL AS 'null' CSV HEADER`);
+    await pool.query(`COPY photos FROM '${process.env.PHOTOSPATH}' DELIMITER ',' NULL AS 'null' CSV HEADER`);
+    await pool.query(`COPY features FROM '${process.env.FEATURESPATH}' DELIMITER ',' NULL AS 'null' CSV HEADER`);
+
+    console.log('Database schema and data imported successfully.');
   } catch (error) {
     console.error('Error building the database schema:', error);
   } finally {
